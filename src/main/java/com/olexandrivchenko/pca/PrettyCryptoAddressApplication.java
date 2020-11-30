c230727d7171b0599b54c45a1b067435c0f4cded
olexandr-ivchenko <olexandr.ivchenko@gmail.com>
2020-11-30 10:44:59 +0200

    Init

---
+++ src/main/java/com/olexandrivchenko/pca/PrettyCryptoAddressApplication.java
@@ -0,0 +1,13 @@
+package com.olexandrivchenko.pca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyCryptoAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyCryptoAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olexandrivchenko/pca/PrettyCryptoAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olexandrivchenko.pca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettyCryptoAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
